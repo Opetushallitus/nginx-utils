abdf0e111c0eb75f51337d7f13fa7576684918ca
Mikko Apo <mikko.apo@reaktor.fi>
2016-02-03 17:51:21 +0200

    Improved performance test

--- performance_test.sh
+++ performance_test.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+
 set -e
+
+function reset() {
+  openresty -s reload
+  sleep 0.1
+  curl http://127.0.0.1:20100/proxypass > /dev/null
+}
+
 rm -rf logs/*
-ab -n1000 http://127.0.0.1:20100/proxypass
 
-ab -n1000 -C CSRF=pow http://127.0.0.1:20100/proxypass
+echo "***************************************************************"
+echo Direct access without LUA
+reset
+curl http://127.0.0.1:20100/proxypass_direct > /dev/null
+ab -n1000 -c20 -C CSRF=pow http://127.0.0.1:20100/proxypass_direct
 
-ab -n1000 -C CSRF=pow http://127.0.0.1:20100/proxypass_direct
+echo "***************************************************************"
+echo Regular GET
+reset
+ab -n1000 -c20 http://127.0.0.1:20100/proxypass
+
+echo "***************************************************************"
+echo GET with CSRF COOKIE
+reset
+ab -n1000 -c20 -C CSRF=pow http://127.0.0.1:20100/proxypass
+
+echo "***************************************************************"
+echo POST with CSRF mismatch
+reset
+ab -n1000 -c20 -C CSRF=pow -p $0 -T application/x-www-form-urlencoded http://127.0.0.1:20100/proxypass
+
+echo "***************************************************************"
+echo POST with CSRF cookie and header
+reset
+ab -n1000 -c10 -C CSRF=pow -H CSRF:pow -p $0 -T application/x-www-form-urlencoded http://127.0.0.1:20100/proxypass
